a5c67fe89d5a56b371bb3023e3ab93a157d303cd
Gabriel Coelho <gabrielcoelhodev1995@gmail.com>
2022-07-20 15:37:45 -0300

    função getCharacter foi testada

--- tests/getCharacter.spec.js
+++ tests/getCharacter.spec.js
@@ -43,13 +43,30 @@ Retorno:
 
 describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
   it('Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {
-    fail('Teste vazio!');
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    expect(getCharacter()).toBeUndefined();
     // Teste se a função retorna o objeto correto para o parâmetro 'Arya',
+    expect(getCharacter('Arya').name).toMatch('Arya Stark');
+    expect(getCharacter('Arya').class).toMatch('Rogue');
+    expect(getCharacter('Arya').phrases).toEqual([ 'Not today', 'A girl has no name.' ]);
     // Teste se a função retorna o objeto correto para o parâmetro 'Brienne',
+    expect(getCharacter('Brienne').name).toMatch('Brienne Tarth');
+    expect(getCharacter('Brienne').class).toMatch('Knight');
+    expect(getCharacter('Brienne').phrases).toEqual([
+      'Im No Lady, Your Grace.',
+      'I, Brienne Of Tarth, Sentence You To Die.'
+    ]);
     // Teste se a função retorna o objeto correto para o parâmetro 'Melissandre',
+    expect(getCharacter('Melissandre').name).toMatch('Melissandre');
+    expect(getCharacter('Melissandre').class).toMatch('Necromancer');
+    expect(getCharacter('Melissandre').phrases).toEqual([
+      'Death By Fire Is The Purest Death.',
+      'For The Night Is Dark And Full Of Terrors.'
+    ]);
     // Teste se o parâmetro não é Case Sensitive, ou seja, independente de conter letras maiúsculas ou minúsculas retorna o mesmo objeto relativo a ele.
+   expect(getCharacter('MELISSANDRE')).toEqual({name: 'Melissandre', class: 'Necromancer', phrases: [ 'Death By Fire Is The Purest Death.', 'For The Night Is Dark And Full Of Terrors.']});
     // Teste se ao passar um nome que não está na tabela, a função retorna undefined.
+    expect(getCharacter('Qualquer')).toBeUndefined();
   });
 });
